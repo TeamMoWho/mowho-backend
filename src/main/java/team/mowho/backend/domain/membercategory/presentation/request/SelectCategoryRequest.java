2cc3f6afc773b16b7c3c166276376a51c4dee189
y22jun <tlsdpwns0612@naver.com>
2026-06-14 16:24:28 +0900

    Feat: SelectCategoryRequest categoryId 요소 단위 검증 추가

--- src/main/java/team/mowho/backend/domain/membercategory/presentation/request/SelectCategoryRequest.java
+++ src/main/java/team/mowho/backend/domain/membercategory/presentation/request/SelectCategoryRequest.java
@@ -1,6 +1,8 @@
 package team.mowho.backend.domain.membercategory.presentation.request;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import team.mowho.backend.domain.membercategory.application.dto.request.SelectCategoryServiceRequest;
@@ -12,7 +14,11 @@ public record SelectCategoryRequest(
 
         @NotEmpty(message = "카테고리를 1개 이상 선택해주세요.")
         @Size(max = 25, message = "카테고리는 최대 25개까지 선택 가능합니다.")
-        List<Long> categoryIds
+        List<
+                @NotNull(message = "카테고리 ID는 null일 수 없습니다.")
+                @Positive(message = "카테고리 ID는 1 이상이어야 합니다.")
+                        Long
+                > categoryIds
 
 ) {
 
